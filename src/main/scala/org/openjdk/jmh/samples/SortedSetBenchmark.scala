2fd8135e07dc16caead18ab37f5bb91a0e373824
Martin Grotzke <martin.grotzke@googlemail.com>
2016-05-20 15:16:30 +0200

    Add SortedSet benchmark for "filterNot" vs "-"
    In sbt running `jmh:run -i 10 -wi 10 -f1 -t1 .*SortedSetBenchmark`
    returned
    
        [info] Benchmark                      Mode  Cnt       Score      Error  Units
        [info] SortedSetBenchmark.filterNot  thrpt   10      74.498 ±    1.946  ops/s
        [info] SortedSetBenchmark.minus      thrpt   10  290330.450 ± 8878.404  ops/s

---
+++ src/main/scala/org/openjdk/jmh/samples/SortedSetBenchmark.scala
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2005, 2013, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package org.openjdk.jmh.samples
+
+import org.openjdk.jmh.annotations._
+
+import scala.collection.immutable.SortedSet
+import scala.util.Random
+
+@State(Scope.Thread)
+class SortedSetBenchmark {
+
+  import SortedSetBenchmark._
+
+  private var items = SortedSet((1 to 10000).map(i => Item(i.toString, i % 10)):_ *)
+
+  @Benchmark
+  def filterNot(): Unit = {
+    val id = Random.nextInt(Count)
+    val idString = id.toString
+    items = items.filterNot(_.id == idString)
+    items += Item(idString, id % 10)
+  }
+
+  @Benchmark
+  def minus(): Unit = {
+    val id = Random.nextInt(Count)
+    val item = Item(id.toString, id % 10)
+    items -= item
+    items += item
+  }
+
+}
+
+object SortedSetBenchmark {
+
+  implicit val ItemOrdering: Ordering[Item] = Ordering.by(item => (item.priority, item.id))
+
+  val Count = 10000
+
+}
+
+case class Item(id: String, priority: Int)
